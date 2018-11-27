03d2ba139f8c24c7b456d18a262c792685c142fe
ftansari <freditansari@gmail.com>
2018-11-27 15:54:28 +0700

    dynamic list tutorial

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -7,11 +7,15 @@ class App extends Component {
 
 state={
   persons:[
-    {name : 'Jimbo', age : 50},
-    {name : 'Jono', age : 45},
-    {name : 'lindesy', age : 50}
-  ]
+    {id: '123', name : 'Jimbo', age : 50},
+    {id: '321', name : 'Jono', age : 45},
+    {id: '456', name : 'lindesy', age : 50}
+  ],
+  showPersons : false
 }
+
+
+
 
 swicthNameHandler= (newName)=>{
   console.log('switchname handler called');
@@ -21,11 +25,93 @@ swicthNameHandler= (newName)=>{
       {name : newName, age : 100},
       {name : 'Jono', age : 45},
       {name : 'lindsay', age : 30}
-    ]
+    ],
+    showPersons:false
   });
 }
 
+deletePersonHandler =(personIndex)=>{
+  //not being used anymore. we have to copy the state then cut the index
+  // const persons = this.state.persons;
+  const persons = [...this.state.persons];
+  persons.splice(personIndex,1);
+  this.setState({persons:persons});
+}
+
+//this one is for two way binding tutorial.
+//event.target value is to get the change from the person.js
+nameChangedHandler = (event, id) =>{
+  console.log('namechanged handler called');
+
+  const personIndex = this.state.persons.findIndex(p =>{
+    return p.id=== id;
+  });
+
+  const person = {
+    ...this.state.persons[personIndex]
+  };
+  //we can also to this as an alternative:
+  //const person = Object.assign({}, this.state.persons[personIndex]);
+
+  person.name = event.target.value;
+  const persons = [ ...this.state.persons];
+  persons[personIndex]= person;
+
+  this.setState({persons:persons});
+}
+
+togglePersonsHandler=()=>{
+
+  console.log('toggleperson handler called');
+  const doesShow = this.state.showPersons;
+  this.setState({showPersons: !doesShow});
+
+}
+
   render() {
+
+    const style = {
+      backgroundColor : 'white',
+      font : 'inherit',
+      border : '1px solid blue',
+      padding : '8 px',
+      cursor : 'pointer'
+    
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons){
+          persons=(
+            <div>
+              {this.state.persons.map((person, index)=>{
+                // return <Person click={this.deletePersonHandler(index)} name={person.name} age = {person.age} />
+                return <Person 
+                click={()=>this.deletePersonHandler(index)} 
+                name={person.name} 
+                age = {person.age} 
+                key={person.id}
+                changed={(event)=>this.nameChangedHandler(event, person.id)} />
+              })}
+                {/* <Person 
+                name = {this.state.persons[0].name} 
+                age ={this.state.persons[0].age} />
+                
+                <Person 
+                name = {this.state.persons[1].name} 
+                age ={this.state.persons[1].age}
+                click = {this.swicthNameHandler.bind(this, 'bomburgess')} 
+                changed={this.nameChangedHandler}> Hobby : ngetrek 
+                </Person>
+                
+                <Person 
+                name = {this.state.persons[2].name} 
+                age ={this.state.persons[2].age} /> */}
+            </div>
+          );
+    }
+
+
     // return React.createElement('div', {className : 'App'}, React.createElement('h1', null, 'hello there! General Kenobi!'));
     return (
       <div className="App">
@@ -47,20 +133,13 @@ swicthNameHandler= (newName)=>{
           {/* you can do this: 
           this is how you pass value into other method in project
           <button onClick={this.swicthNameHandler.bind(this, 'johannesburgess')}>click me</button> */}
-           <button onClick={() => this.swicthNameHandler('you can also do this!!')}>click me</button>
-          <Person 
-          name = {this.state.persons[0].name} 
-          age ={this.state.persons[0].age} />
+           
+           {/* using the inline style defined above
+          <button style={style} onClick={() => this.swicthNameHandler('you can also do this!!')}>click me</button> */}
+          <button style={style} onClick={this.togglePersonsHandler}>click me</button>
+          {persons}
+       
           
-          <Person 
-          name = {this.state.persons[1].name} 
-          age ={this.state.persons[1].age}
-          click = {this.swicthNameHandler.bind(this, 'bomburgess')} > Hobby : ngetrek 
-          </Person>
-          
-          <Person 
-          name = {this.state.persons[2].name} 
-          age ={this.state.persons[2].age} />
         </header>
       
       </div>
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,9 +1,11 @@
 import React from 'react';
+//import css to component
+import './Person.css'
 
 const person =(props)=>{
     // return <p>I'm a person and i'm {Math.floor(Math.random() * 100)} years old</p>
     return (
-        <div>
+        <div className='Person'>
             {/* props.click is how you call method from another components.
                     props.click is defined in the second Person tag in App.js
 
@@ -11,6 +13,9 @@ const person =(props)=>{
             */}
             <p onClick={props.click}>I'm {props.name} and i'm {props.age} years old</p>
             <p> {props.children}</p>
+
+            {/* two way bindings tutorial */}
+            <input type="text" onChange ={props.changed} value={props.name}></input>
         </div>
     )
 };
